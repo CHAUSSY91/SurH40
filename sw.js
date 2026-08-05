cc73860f9dfc999c517d91f892d6de09614782cb
CHAUSSY91 <mrik9111@gmail.com>
2026-08-05 17:27:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE='surh40-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest',
+  './icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))
+    .then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>{try{c.put(e.request,copy);}catch(err){}});
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
+self.addEventListener('notificationclick',e=>{
+  e.notification.close();
+  e.waitUntil(clients.matchAll({type:'window'}).then(l=>{
+    for(const c of l)if('focus' in c)return c.focus();
+    if(clients.openWindow)return clients.openWindow('./index.html');
+  }));
+});
