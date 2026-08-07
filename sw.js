361a690fe850fdf6c5c7bfb63c449e0d24be6b55
CHAUSSY91 <mrik9111@gmail.com>
2026-08-07 20:18:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='surh40-v3.3';
+const CACHE='surh40-v3.5';
 const ASSETS=['./','./index.html','./manifest.webmanifest',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{
